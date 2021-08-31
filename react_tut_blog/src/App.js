d1e4776274510e1462a2239315dbd03438ec8e60
jawda <jawda1210@gmail.com>
2021-08-31 17:38:12 -0500

    feat (React): added a page not found page

--- react_tut_blog/src/App.js
+++ react_tut_blog/src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 // this is not hmtl it is jsx (which is why it looks differently), babel converts it and renders it to the dom
 //this is also the root component all others will be nested inside of it.
@@ -25,6 +26,9 @@ function App() {
           {/* passing in paramaters into the route  */}
           <Route path="/blogs/:id">
             <BlogDetails />
+          </Route>
+          <Route>
+            <NotFound path="*"/>
           </Route>
         </Switch>
       </div>
