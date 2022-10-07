02f4d762c1674cb036861e119ab4e2b8e94f5000
joew <joew@quavermusic.com>
2022-10-07 10:19:50 -0500

    feat (wheel_of_decks): updated styles to make font sizes a bit larger, removed editable text, moved wheel into new js file

--- wheel-of-decks/src/App.js
+++ wheel-of-decks/src/App.js
@@ -1,91 +1,21 @@
 import React from 'react';
 import './App.css';
+import Wheel from './pages/wheel';
+import './wheel.scss';
 
-class App extends React.Component {
-  state = {
-    name: "circle"
-  }
-  startRotation = () => {
-    this.setState({
-      name: "circle start-rotate"
-    });
-    setTimeout(() => {
-      this.setState({
-        name: "circle start-rotate stop-rotate"
-      });
-    }, Math.floor(Math.random() * 10000) + 1);
-  }
-  render() {
-    return(
-      <div>
-        <div className='arrow'></div>
-      <ul className={this.state.name}>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">1</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">2</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">3</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">4</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">5</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">6</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">7</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">8</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">9</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">10</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">11</div>
-        </li>
-        <li>
-          <div className="text"
-            contentEditable="true" 
-            spellCheck="false">12</div>
-        </li>
-      </ul>
-      <button className='spin-button'
-       onClick={this.startRotation}>SPIN</button>
+function App()  {
+
+      return(
+      
+      <div className="App">
+      <header className="App-header">
+         
+      </header>
+      
+      <Wheel />
     </div>
   );
-  }
+  
   
 }
 
---
+++ wheel-of-decks/src/pages/wheel.js
@@ -0,0 +1,80 @@
+import React from "react";
+class Wheel extends React.Component{
+    // constructor(){
+    //     super();
+    // }
+    state = {
+          name: "circle"
+        }
+        startRotation = () => {
+          this.setState({
+            name: "circle start-rotate"
+          });
+          setTimeout(() => {
+            this.setState({
+              name: "circle start-rotate stop-rotate"
+            });
+          }, Math.floor(Math.random() * 5000) + 1);
+        }
+    render() {
+        return(
+            <div>
+        <div className='arrow'></div>
+      <ul className={this.state.name}>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">1</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">2</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">3</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">4</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">5</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">6</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">7</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">8</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">9</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">10</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">11</div>
+        </li>
+        <li className='li-rotate'>
+          <div className="text"
+            spellCheck="false">12</div>
+        </li>
+      </ul>
+      <button className='spin-button'
+       onClick={this.startRotation}>SPIN</button>
+    </div>
+        )
+    }
+}
+
+export default Wheel;
